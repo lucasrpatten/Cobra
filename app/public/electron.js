b5abf30f3df23b42e46843acf6d37724d163d7a0
lucasrpatten <lucasrpattendev@gmail.com>
2022-11-09 17:31:24 -0700

    removed pydists

--- app/public/electron.js
+++ app/public/electron.js
@@ -23,12 +23,6 @@ function createWindow() {
     }
   });
 
-
-  // communication api
-
-
-
-
   mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
   mainWindow.on('closed', () => mainWindow = null);
 }
